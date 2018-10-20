bb3bf2dc53147d849fd40a7a1b6a1893f4e9e833
basketballrelativity <patmcfarla@gmail.com>
2018-10-20 19:49:53 -0400

    Adding the Team class

--- py_ball/team.py
+++ py_ball/team.py
@@ -1,68 +1,67 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 16 18:47:46 2018
+Created on Sat Oct 20 18:34:11 2018
 
 @author: patrickmcfarlane
 
-player.py contains the Player class that
-enables API calls for player-related
+team.py contains the Team class that
+enables API calls for team-related
 endpoints
 """
 
 from __init__ import api_call, parse_api_call
 
-class Player:
-    """ The Player class contains all resources needed
-    to use the player-related API calls. stats.nba.com
-    has the following player performance stats related API endpoints:
-        - playercareerstats: Season and career stats for a given player,
-        broken down by season type (including college).
-        - playercompare: Individual and combined (overall) statistics
-        for a given list of players and opposing players.
-        - playerdashboardbyclutch: Traditional and rank statistics broken
-        down by different definitions of clutch for a player.
-        - playerdashboardbygamesplits: Traditional and rank statistics
+class Team:
+    """ The Team class contains all resources needed
+    to use the team-related API calls. stats.nba.com
+    has the following  team-related API endpoints:
+        - teamdashboardbyclutch: Traditional and rank statistics broken
+        down by different definitions of clutch for a team.
+        - teamdashboardbygamesplits: Traditional and rank statistics
         broken down by different splits (half, quarter, and score
         differential).
-        - playerdashboardbygeneralsplits: Traditional and rank statistics
+        - teamdashboardbygeneralsplits: Traditional and rank statistics
         broken down by different splits (win/loss, location, month,
         pre/post All-Star, days rest). 
-        - playerdashboardbylastngames: Traditional and rank statistics
+        - teamdashboardbylastngames: Traditional and rank statistics
         broken down by the number of n recent games and game number
         bins.
-        - playerdashboardbyopponent: Traditional and rank statistics
+        - teamdashboardbyopponent: Traditional and rank statistics
         broken down by opponent splits (conference, division, and
         individual team).
-        - playerdashboardbyshootingsplits: Traditional and rank statistics
+        - teamdashboardbyshootingsplits: Traditional and rank statistics
         broken down by shooting splits (shot distance, shot area,
         assisted/unassisted, shot type, and indivdual assistant).
-        - playerdashboardbyteamperformance: Traditional and rank statistics
+        - teamdashboardbyteamperformance: Traditional and rank statistics
         broken down by team performance splits (win/loss, score differential,
         points for, and points against).
-        - playerdashboardbyyearoveryear: Traditional and rank statistics
+        - teamdashboardbyyearoveryear: Traditional and rank statistics
         broken down by year.
-        - playerdashptpass: Shooting statistics for passes to and from
+        - teamdashlineups: Traditional and plus/minus statistics
+        for sets of lineups between sizes 2 to 5 players, inclusive.
+        - teamdashptpass: Shooting statistics for passes to and from
         a player broken down by teammates.
-        - playerdashptreb: Rebound statistics broken down by shot type,
+        - teamdashptreb: Rebound statistics broken down by shot type,
         contesting players, and shot/rebound distance.
-        - playerdashptshotdefend: Defensive statistics broken down
-        by shot type/distance.
-        - playerdashptshots: Shooting statistics broken down by shot
+        - teamdashptshots: Shooting statistics broken down by shot
         type, shot clock time, number of tribbles, defender proximity, and
         length of touch.
-        - playergamelog: Game log statistics for a given year.
-        - playerprofilev2: Career and season summary statistics broken down
-        by season type.
-        - playervsplayer: Player statistics versus a given opponent player
+        - teamgamelog: Game log statistics for a given year.
+        - teaminfocommon: Team information for a given year.
+        - teamplayerdashboard: Player traditional and rank statistics
+        for a given team.
+        - teamplayeronoffdetails: Team traditional and rank statistics
+        broken down by on/off splits per player.
+        - teamplayeronoffsummary: Team summary statistics broken down
+        by on/off splits per player.
+        - teamvsplayer: Team statistics versus a given opponent player
         broken down by several shooting related splits (shot distance and
         area)
-        - playersvsplayers: Currently not available
-        - shotchartdetail: Player and league shot chart data giving results
-        and location-related information for several player actions, 
-        such as shots and fouls.
+        - teamyearbyyearstats: Team statistics and performance broken down
+        by year.
 
-    The Player class has the following required parameters:
+    The Team class has the following required parameters:
 
         @param league_id (LeagueID in the API): String of a two-digit
         number corresponding to the league. '00' is the NBA, '10' is
@@ -70,16 +69,6 @@ class Player:
 
         @param player_id (PlayerID in the API): String of an integer
         corresponding to a player ID for a given player.
-
-        @param player_id_list (PlayerIDList in the API): String of a
-        comma-separated list of player IDs for player comparisons to the
-        players given in vs_player_id_list. Valid only for
-        the playercompare endpoint.
-
-        @param vs_player_id_list (VsPlayerIDList in the API): String of a
-        comma-separated list of player IDs for player comparisons to the
-        players given in player_id_list. Valid only for the
-        playercompare endpoint.
 
         @param per_mode (PerMode in the API): String indicating the type
         of rate stats to be returned. Valid values include:
@@ -183,41 +172,6 @@ class Player:
         @param vs_player_id (VsPlayerID in the API): String of an integer
         corresponding to a player ID for a given player.
 
-        @param player_team_id (PlayerTeamID in the API): String of an integer
-        corresponding to a team ID for a given team.
-
-        @param vs_player_team_id (VsPlayerTeamID in the API): String of 
-        an integer corresponding to a team ID for a given team.
-
-        @param player_id_x (PlayerIDX in the API): String of an integer
-        corresponding to a player ID for a given player. The x (X in the
-        API) is an integer 1 through 5. Valid only for the playersvsplayers
-        endpoint.
-
-        @param vs_player_id_x (VsPlayerIDX in the API): String of an integer
-        corresponding to a player ID for a given player. The x (X in the
-        API) is an integer 1 through 5. Valid only for the playersvsplayers
-        endpoint.
-
-        @param rookie_year (RookieYear in the API): of a two-year
-        season in a YYYY-ZZ format, where the ZZ are the last two
-        digits of the following year. For example, '2017-18' is a valid
-        value of Season and represents the 2017-18 NBA season. This field
-        should correspond to the rookie year of the player given.
-
-        @param context_measure (ContextMeasure in the API): String of an
-        abbreviated statistic corresponding to the type of action to be
-        included in the data returned. Valid values include:
-            - 'PTS', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT',
-            'PF', 'EFG_PCT', 'TS_PCT', 'PTS_FB', 'PTS_OFF_TOV',
-            'PTS_2ND_CHANCE', 'PF'
-
-        @param player_position (PlayerPosition in the API): String of
-        a basketball position corresponding to the position of the player
-        given. An empty string returns data across all positions played.
-        Valid values include:
-            - 'Guard', 'Center', 'Forward'
-
         @param game_id (GameID in the API): 10-digit string that represents
         a unique game. The format is two leading zeroes, followed by a
         season indicator number ('1' for preseason, '2' for regular season),
@@ -226,6 +180,11 @@ class Player:
         5 digits increment from '00001' in order as the season progresses.
         For example, '0021600001' is the GameID of the first game of the
         2016-17 NBA regular season.
+
+        @param group_quantity (GroupQuantity in the API): String of
+        an integer indicating the number of players to include a
+        lineup for the leaguedashlineups endpoint. The minimum value
+        is '1' and the maximum value is '5'.
 
     Attributes:
 
@@ -239,34 +198,24 @@ class Player:
         key to a list of dictionaries containing the corresponding data.
     """
 
-    def __init__(self, endpoint='playercareerstats',
+    def __init__(self, endpoint='teamdashboardbyclutch',
                  player_id='2544',
-                 player_id_list='2544', vs_player_id_list='201939',
                  league_id='00',
                  per_mode='PerGame', plus_minus='N',
                  rank='Y', pace_adjust='N',
                  measure_type='Base', period='0',
                  vs_conference='', last_n_games='0',
-                 team_id='0', location='', outcome='',
+                 team_id='1610612747', location='', outcome='',
                  date_from='', date_to='', opp_team_id='0',
                  season='2017-18', vs_division='',
                  game_segment='', month='0',
                  season_type='Regular Season', season_segment='',
                  vs_player_id='201939',
-                 player_team_id='1610612747',
-                 vs_player_team_id='1610612744',
-                 player_id_1='2544', player_id_2='0',
-                 player_id_3='0', player_id_4='0',
-                 player_id_5='0',
-                 vs_player_id_1 = '201939', vs_player_id_2='0',
-                 vs_player_id_3='0', vs_player_id_4='0',
-                 vs_player_id_5='0', rookie_year='',
-                 context_measure = 'FGA', player_position='',
-                 game_id='0011800079'):
+                 game_id='0011800079', group_quantity='5'):
 
         # Controlling the parameters depending on the endpoint
-        if endpoint not in ['playercareerstats', 'playergamelog',
-                            'playerprofilev2']:
+        if endpoint not in ['teamgamelog', 'teaminfocommon',
+                            'teamyearbyyearstats']:
             params = {'LeagueID': league_id,
                       'PerMode': per_mode,
                       'PlusMinus': plus_minus,
@@ -279,7 +228,7 @@ class Player:
                       'Outcome': outcome,
                       'DateFrom': date_from,
                       'DateTo': date_to,
-                      'PlayerID': player_id,
+                      'TeamID': team_id,
                       'OpponentTeamID': opp_team_id,
                       'Season': season,
                       'VsDivision': vs_division,
@@ -288,52 +237,26 @@ class Player:
                       'SeasonType': season_type,
                       'SeasonSegment': season_segment,
                       'LastNGames': last_n_games}
-        elif endpoint in  ['playercareerstats',
-                          'playerprofilev2']:
-            params = {'PlayerID': player_id,
-                      'PerMode': per_mode}
-        elif endpoint == 'playergamelog':
-            params = {'PlayerID': player_id,
+        elif endpoint in ['teamgamelog', 'teaminfocommon']:
+            params = {'LeagueID': league_id,
+                      'TeamID': team_id,
                       'Season': season,
                       'SeasonType': season_type}
-        
+        elif endpoint in ['teamyearbyyearstats']:
+            params = {'LeagueID': league_id,
+                      'TeamID': team_id,
+                      'PerMode': per_mode,
+                      'SeasonType': season_type}
 
-        if endpoint in ['playercompare']:
-            params['PlayerIDList'] = player_id_list
-            params['VsPlayerIDList'] = vs_player_id_list
-        elif endpoint in ['playerdashptpass']:
+        if endpoint in ['teamdashlineups']:
+            params['GroupQuantity'] = group_quantity
+            params['GameID'] = game_id
+        elif endpoint in ['teamdashptpass']:
             del params['PlusMinus'], params['PaceAdjust']
             del params['Rank'], params['MeasureType']
             del params['Period'], params['GameSegment']
-            params['TeamID'] = team_id
-        elif endpoint in ['playerdashptreb',
-                          'playerdashptshotdefend',
-                          'playerdashptshots']:
-            del params['PlusMinus'], params['PaceAdjust']
-            del params['Rank'], params['MeasureType']
-            params['TeamID'] = team_id
-        elif endpoint in ['playervsplayer']:
-            params['PlayerID'] = player_id
+        elif endpoint in ['teamvsplayer']:
             params['VsPlayerID'] = vs_player_id
-        elif endpoint in ['playersvsplayers']:
-            params['PlayerTeamID'] = player_team_id
-            params['VsTeamID'] = vs_player_team_id
-            params['PlayerID1'] = player_id_1
-            params['PlayerID2'] = player_id_2
-            params['PlayerID3'] = player_id_3
-            params['PlayerID4'] = player_id_4
-            params['PlayerID5'] = player_id_5
-            params['VsPlayerID1'] = vs_player_id_1
-            params['VsPlayerID2'] = vs_player_id_2
-            params['VsPlayerID3'] = vs_player_id_3
-            params['VsPlayerID4'] = vs_player_id_4
-            params['VsPlayerID5'] = vs_player_id_5
-        elif endpoint in ['shotchartdetail']:
-            params['TeamID'] = team_id
-            params['RookieYear'] = rookie_year
-            params['ContextMeasure'] = context_measure
-            params['PlayerPosition'] = player_position
-            params['GameID'] = game_id
 
         self.api_resp = api_call(endpoint=endpoint,
                                  params=params)
