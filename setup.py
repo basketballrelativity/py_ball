d093d6f994b99d4154f0f7f52758927929321ea8
basketballrelativity <patmcfarla@gmail.com>
2022-09-28 18:39:15 -0400

    updating package

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py_ball",
-    version="1.1",
+    version="1.4",
     author="Patrick McFarlane & Avyay Varadarajan",
     author_email="patmcfarla@gmail.com",
     description="Python API wrapper for stats.nba.com with a focus on \
